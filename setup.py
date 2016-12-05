6d6ca09374e48059cf586837bf5732ba10e58cb1
Joshua Arnott <josh@snorfalorpagus.net>
2016-12-05 16:16:14 +0000

    Added conda recipe

--- setup.py
+++ setup.py
@@ -14,6 +14,19 @@ with open(os.path.join(os.path.dirname(__file__), "pycatchmod", "__init__.py"), 
             _, version, _ = line.split("\"")
             break
 
+# needed to compile
+setup_requires = [
+    "cython", "numpy",
+]
+# needed to run
+install_requires = [
+    "numpy", "pandas", "click", "tables", "xlrd"
+]
+# only needed for testing
+test_requires = [
+    "pytest"
+]
+
 setup(
     name='pycatchmod',
     version=version,
@@ -21,9 +34,9 @@ setup(
     author='James E Tomlinson',
     author_email='tomo.bbe@gmail.com',
     packages=['pycatchmod', "pycatchmod.io"],
-    install_requires=['cython', 'numpy', 'pandas', 'click', 'tables', 'xlrd'],
-    setup_requires=['cython', 'numpy'],
-    tests_require=['pytest'],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=test_requires,
     ext_modules=[catchmod, weather_generator],
     cmdclass = {'build_ext': build_ext},
     entry_points={
